93bf733f269d5594284806162acf4e4c5d3e63f5
Cody Peterson <54814569+lostmygithubaccount@users.noreply.github.com>
2019-12-13 16:50:40 -0800

    updates

--- dask/startDask.py
+++ dask/startDask.py
@@ -11,8 +11,6 @@ import subprocess
 import socket
 
 from notebook.notebookapp import list_running_servers
-
-
 # -
 
 def flush(proc, proc_log):
@@ -33,7 +31,7 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--datastore")
-    parser.add_argument("--jupyter_token", default=uuid.uuid1().hex)
+    #parser.add_argument("--jupyter_token", default=uuid.uuid1().hex)
     parser.add_argument("--script")
 
     args, unparsed = parser.parse_known_args()
@@ -65,30 +63,27 @@ if __name__ == '__main__':
     
     if rank == 0:
         Run.get_context().log("headnode", ip)
-        Run.get_context().log("cluster",
-                              "scheduler: {scheduler}, dashboard: {dashboard}".format(scheduler=scheduler,
-                                                                                      dashboard=dashboard))
+        Run.get_context().log("cluster", "scheduler: {scheduler}, dashboard: {dashboard}".format(scheduler=scheduler, dashboard=dashboard))
         Run.get_context().log("datastore", args.datastore)
 
-        cmd = ("jupyter lab --ip 0.0.0.0 --port 8888" + \
-                          " --NotebookApp.token={token}" + \
-                          " --allow-root --no-browser").format(token=args.jupyter_token)
-        jupyter_log = open("jupyter_log.txt", "a")
-        jupyter_proc = subprocess.Popen(cmd.split(), universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-
-        jupyter_flush = threading.Thread(target=flush, args=(jupyter_proc, jupyter_log))
-        jupyter_flush.start()
-
-        while not list(list_running_servers()):
-            time.sleep(5)
-
-        jupyter_servers = list(list_running_servers())
-        assert (len(jupyter_servers) == 1), "more than one jupyter server is running"
-
-        Run.get_context().log("jupyter",
-                              "ip: {ip_addr}, port: {port}".format(ip_addr=ip, port=jupyter_servers[0]["port"]))
-        Run.get_context().log("jupyter-token", jupyter_servers[0]["token"])
-
+#        cmd = ("jupyter lab --ip 0.0.0.0 --port 8888" + \
+#                          " --NotebookApp.token={token}" + \
+#                          " --allow-root --no-browser").format(token=args.jupyter_token)
+#        jupyter_log = open("jupyter_log.txt", "a")
+#        jupyter_proc = subprocess.Popen(cmd.split(), universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+#
+#        jupyter_flush = threading.Thread(target=flush, args=(jupyter_proc, jupyter_log))
+#        jupyter_flush.start()
+#
+#        while not list(list_running_servers()):
+#            time.sleep(5)
+#
+#        jupyter_servers = list(list_running_servers())
+#        assert (len(jupyter_servers) == 1), "more than one jupyter server is running"
+#
+#        Run.get_context().log("jupyter","ip: {ip_addr}, port: {port}".format(ip_addr=ip, port=jupyter_servers[0]["port"]))
+#        Run.get_context().log("jupyter-token", jupyter_servers[0]["token"])
+#
         cmd = "dask-scheduler " + "--port " + scheduler.split(":")[1] + " --dashboard-address " + dashboard
         scheduler_log = open("scheduler_log.txt", "w")
         scheduler_proc = subprocess.Popen(cmd.split(), universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -106,7 +101,7 @@ if __name__ == '__main__':
             exit_code = os.system(command_line)
             print('process ended with code', exit_code)
             print('killing scheduler, worker and jupyter')
-            jupyter_proc.kill()
+            #jupyter_proc.kill()
             scheduler_proc.kill()
             worker_proc.kill()
             exit(exit_code)
