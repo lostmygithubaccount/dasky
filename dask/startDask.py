cb72cdc72f9098bcd8434719024c3a1fca191df2
Cody Peterson <54814569+lostmygithubaccount@users.noreply.github.com>
2019-12-17 13:48:23 -0800

    big commits

--- dask/startDask.py
+++ dask/startDask.py
@@ -1,4 +1,4 @@
-# +
+### Imports
 from mpi4py import MPI
 import os
 import argparse
@@ -11,7 +11,6 @@ import subprocess
 import socket
 
 from notebook.notebookapp import list_running_servers
-# -
 
 def flush(proc, proc_log):
     while True:
@@ -24,15 +23,19 @@ def flush(proc, proc_log):
             proc_log.write(proc_out)
             proc_log.flush()
 
-
 if __name__ == '__main__':
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--datastore")
-    #parser.add_argument("--jupyter_token", default=uuid.uuid1().hex)
+    parser.add_argument("--jupyter_token", default=uuid.uuid1().hex)
     parser.add_argument("--script")
+
+    parser.add_argument("--dashboard_port", default=9797)
+    parser.add_argument("--jupyter_port", default=9999)
+    parser.add_argument("--ci_hostname")
+    parser.add_argument("--ci_ip")
 
     args, unparsed = parser.parse_known_args()
     
@@ -62,28 +65,39 @@ if __name__ == '__main__':
     print("- my ip is ", ip)
     
     if rank == 0:
-        Run.get_context().log("headnode", ip)
-        Run.get_context().log("cluster", "scheduler: {scheduler}, dashboard: {dashboard}".format(scheduler=scheduler, dashboard=dashboard))
-        Run.get_context().log("datastore", args.datastore)
+        Run.get_context().log('headnode', ip)
+        Run.get_context().log('scheduler', scheduler) 
+        Run.get_context().log('dashboard', dashboard)
+        Run.get_context().log('datastore', args.datastore)
 
-#        cmd = ("jupyter lab --ip 0.0.0.0 --port 8888" + \
-#                          " --NotebookApp.token={token}" + \
-#                          " --allow-root --no-browser").format(token=args.jupyter_token)
-#        jupyter_log = open("jupyter_log.txt", "a")
-#        jupyter_proc = subprocess.Popen(cmd.split(), universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-#
-#        jupyter_flush = threading.Thread(target=flush, args=(jupyter_proc, jupyter_log))
-#        jupyter_flush.start()
-#
-#        while not list(list_running_servers()):
-#            time.sleep(5)
-#
-#        jupyter_servers = list(list_running_servers())
-#        assert (len(jupyter_servers) == 1), "more than one jupyter server is running"
-#
-#        Run.get_context().log("jupyter","ip: {ip_addr}, port: {port}".format(ip_addr=ip, port=jupyter_servers[0]["port"]))
-#        Run.get_context().log("jupyter-token", jupyter_servers[0]["token"])
-#
+        cmd = ("jupyter lab --ip 0.0.0.0 --port 8888" + \
+                          " --NotebookApp.token={token}" + \
+                          " --allow-root --no-browser").format(token=args.jupyter_token)
+        jupyter_log = open("jupyter_log.txt", "a")
+        jupyter_proc = subprocess.Popen(cmd.split(), universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+        jupyter_flush = threading.Thread(target=flush, args=(jupyter_proc, jupyter_log))
+        jupyter_flush.start()
+
+        while not list(list_running_servers()):
+            time.sleep(5)
+
+        jupyter_servers = list(list_running_servers())
+        assert (len(jupyter_servers) == 1), "more than one jupyter server is running"
+
+        dashboard_forward = subprocess.Popen(["/usr/bin/socat", 
+                             	 "tcp-listen:{},fork".format(8787), 
+                             	 "tcp:{}:{}".format(args.ci_ip, args.dashboard_port)],
+                            	 stderr=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+ 
+        jupyter_forward = subprocess.Popen(["/usr/bin/socat", 
+                             	 "tcp-listen:{},fork".format(8888), 
+                                 "tcp:{}:{}".format(args.ci_ip, args.jupyter_port)],
+                            	 stderr=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+ 
+        Run.get_context().log('dashboard-url', 'https://{}-{}.{}.instances.azureml.net/status'.format(args.ci_hostname, args.dashboard_port, Run.get_context().experiment.workspace.get_details()['location']))
+        Run.get_context().log('jupyter-url', 'https://{}-{}.{}.instances.azureml.net/lab?token={}'.format(args.ci_hostname, args.jupyter_port, Run.get_context().experiment.workspace.get_details()['location'], args.jupyter_token))
+
         cmd = "dask-scheduler " + "--port " + scheduler.split(":")[1] + " --dashboard-address " + dashboard
         scheduler_log = open("scheduler_log.txt", "w")
         scheduler_proc = subprocess.Popen(cmd.split(), universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
